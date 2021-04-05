562b212b923df9eeda3a0af38ac86c583ff3cb58
melnee <melanie.neilan@gmail.com>
2021-04-04 19:26:43 -0700

    deal with invalid search results for stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,11 +5,12 @@ class Stock < ApplicationRecord
             secret_token: Rails.application.credentials.iex_client[:sandbox_secret_key],
             endpoint: 'https://sandbox.iexapis.com/v1'
         )
-        #now return the price
-        #inside the class, the Stock.new is implied, here you only have to write new
-        #in the name param, you're finding the company by the ticker symbol
-        #and then chaining that found company to get it's full name
-        new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name,  last_price: client.price(ticker_symbol))
+        #begin and rescue is like try catch
+        begin
+            #now return the price
+            new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name,  last_price: client.price(ticker_symbol))
+        rescue => exception
+            return nil
+        end     
     end
-
 end
